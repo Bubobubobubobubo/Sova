2b569d91d3359f9e3b0f12028728b99335ec8780
TanguyDubois <dubois.tanguy@gmail.com>
2025-12-01 20:29:58 +0100

    Parsing cache, don't know if better or worse

--- core/src/schedule.rs
+++ core/src/schedule.rs
@@ -30,7 +30,7 @@ pub use action_timing::ActionTiming;
 pub use message::SchedulerMessage;
 pub use notification::SovaNotification;
 
-pub const SCHEDULED_DRIFT: SyncTime = 15_000;
+pub const SCHEDULED_DRIFT: SyncTime = 20_000;
 pub const SCHEDULER_ACTIVE_WAITING_SWITCH: SyncTime = 100;
 
 pub struct Scheduler {
